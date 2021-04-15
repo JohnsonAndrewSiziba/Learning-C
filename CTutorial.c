26c00cd1d625e27c4bfcf5386c3abb9ba052b81f
Siziba Johnson A <siziba.uz@outlook.com>
2021-04-15 13:41:28 +0200

    Sasageyo

---
+++ CTutorial.c
@@ -0,0 +1,34 @@
+/* 
+Multi line comment
+*/
+
+//Single line comment
+
+#include <stdio.h> //for input and output
+#include <string.h>
+
+#define MYNAME "Johnson" //constant
+
+int globalVar = 100; // global variable
+
+int main() {
+
+    char firstLeter = 'D'; //character
+
+    int age = 35;
+
+    long int superBig = -3676385872387;
+
+    float piValue = 3.14159;
+
+    double reallyBigPi = 3.14159141591415914159141591415914159;
+
+    printf("\n");
+
+    printf("This will print to screen. \n\n");
+
+}
+
+
+//gcc CTutorial.c
+// ./a.out
